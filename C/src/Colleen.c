3d4425e741af1490a443a76773c56bdc0c9cd913
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-07-03 13:12:15 +0200

    fix comments format in Grace and Colleen

--- C/src/Colleen.c
+++ C/src/Colleen.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 
-// outside comment
+/*
+	outside comment
+*/
 void lol() {}
 
 int main()
 {
-	// inside comment
-	char *s = "#include<stdio.h>%c%c// outside comment%cvoid lol() {}%c%cint main()%c{%c%c// inside comment%c%cchar *s = %c%s%c;%c%cprintf(s, 10, 10, 10, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);%c%clol();%c}%c";
-	printf(s, 10, 10, 10, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);
+	/*
+		inside comment
+	*/
+	char *s = "#include<stdio.h>%c%c/*%c%coutside comment%c*/%cvoid lol() {}%c%cint main()%c{%c%c/*%c%c%cinside comment%c%c*/%c%cchar *s = %c%s%c;%c%cprintf(s, 10, 10, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);%c%clol();%c}%c";
+	printf(s, 10, 10, 10, 9, 10, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);
 	lol();
 }
--- C/src/Grace.c
+++ C/src/Grace.c
@@ -1,4 +1,6 @@
-// commentaire de ouf
+/*
+	commentaire de ouf
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,7 +8,7 @@
 
 #define CHECK_OPEN(FD) { if (FD == -1) \
 							exit(1); }
-#define WRITE(STR) { int fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644); CHECK_OPEN(fd); dprintf(fd, STR, 10, 10, 10, 10, 10, 10, 92, 10, 9, 9, 9, 9, 9, 9, 9, 10, 34, 34, 10, 34, STR, 34, 10, 10, 10); close(fd); }
-#define MAIN int main() { char *s = "// commentaire de ouf%c#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c%c#define CHECK_OPEN(FD) { if (FD == -1) %c%c%c%c%c%c%c%c%cexit(1); }%c#define WRITE(STR) { int fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC, 0644); CHECK_OPEN(fd); dprintf(fd, STR, 10, 10, 10, 10, 10, 10, 92, 10, 9, 9, 9, 9, 9, 9, 9, 10, 34, 34, 10, 34, STR, 34, 10, 10, 10); close(fd); }%c#define MAIN int main() { char *s = %c%s%c; WRITE(s) }%c%cMAIN%c"; WRITE(s) }
+#define WRITE(STR) { int fd = open("Grace_kid.c", O_WRONLY | O_CREAT | O_TRUNC, 0644); CHECK_OPEN(fd); dprintf(fd, STR, 10, 9, 10, 10, 10, 10, 10, 10, 10, 92, 10, 9, 9, 9, 9, 9, 9, 9, 10, 34, 34, 10, 34, STR, 34, 10, 10, 10); close(fd); }
+#define MAIN int main() { char *s = "/*%c%ccommentaire de ouf%c*/%c#include <stdio.h>%c#include <stdlib.h>%c#include <unistd.h>%c#include <fcntl.h>%c%c#define CHECK_OPEN(FD) { if (FD == -1) %c%c%c%c%c%c%c%c%cexit(1); }%c#define WRITE(STR) { int fd = open(%cGrace_kid.c%c, O_WRONLY | O_CREAT | O_TRUNC, 0644); CHECK_OPEN(fd); dprintf(fd, STR, 10, 9, 10, 10, 10, 10, 10, 10, 10, 92, 10, 9, 9, 9, 9, 9, 9, 9, 10, 34, 34, 10, 34, STR, 34, 10, 10, 10); close(fd); }%c#define MAIN int main() { char *s = %c%s%c; WRITE(s) }%c%cMAIN%c"; WRITE(s) }
 
 MAIN
