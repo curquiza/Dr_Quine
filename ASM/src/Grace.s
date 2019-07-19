ba85921f80ce63ee22065bc39f82a3f014a04cba
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-07-19 16:39:03 +0200

    Grace in progress

--- ASM/src/Grace.s
+++ ASM/src/Grace.s
@@ -1,31 +1,50 @@
-; SYSCALL
-%define MACH_SYSCALL(nb)        0x2000000 | nb
-%define STDOUT                  1
-%define WRITE                   4
+; comment
+%define MACH_SYSCALL(nb) 0x2000000 | nb
+%define OPEN 5
+%define OPEN2 5
+%define FILENAME "Grace_kid.s"
 
-section .data
-hello:
-    .string db "Hello World!", 10
-    .len equ $ - hello.string
-
-section .text
-    global start
-    global _main
+%macro MAIN 1
+global _main
+global start
 
 start:
-    call _main
-    ret
+call _main
+ret
 
 _main:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
+push rbp
+mov rbp, rsp
 
-	mov edi, STDOUT
-	lea esi, [rel hello.string]
-    mov rdx, hello.len
-    mov rax, MACH_SYSCALL(WRITE)
-    syscall
+; lea rdi, [rel %1]
+; mov rsi, 1
+; mov rdx, 0644
+; mov rax, MACH_SYSCALL(OPEN)
+; syscall
 
-    leave
-    ret
+lea rdi, [rel %1]
+mov rsi, 0777
+mov rax, MACH_SYSCALL(OPEN2)
+syscall
+
+mov rdi, 1
+lea rsi, [rel code]
+mov rdx, rax
+; mov rdx, 10
+; mov rcx, 34
+; lea r8, [rel code]
+call _dprintf
+
+mov rsp, rbp
+pop rbp
+ret
+%endmacro
+
+section .data
+code: db "CODE => %d", 10, 0
+filename: db FILENAME, 0
+
+section .text
+extern _dprintf
+
+MAIN filename
