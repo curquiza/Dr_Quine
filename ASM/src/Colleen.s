ce71471d74ddfe650ee3b30cfaa1770d64cbbd86
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-07-19 14:14:04 +0200

    Colleen done

--- ASM/src/Colleen.s
+++ ASM/src/Colleen.s
@@ -1,31 +1,30 @@
-; SYSCALL
-%define MACH_SYSCALL(nb)        0x2000000 | nb
-%define STDOUT                  1
-%define WRITE                   4
-
+; commentaire outside
 section .data
-hello:
-    .string db "Hello World!", 10
-    .len equ $ - hello.string
+code: db "; commentaire outside%1$csection .data%1$ccode: db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cglobal start%1$cglobal _main%1$cextern _printf%1$c%1$cstart:%1$ccall _main%1$cret%1$c%1$c_main:%1$c; commentaire inside%1$cpush rbp%1$cmov rbp, rsp%1$c%1$clea rdi, [rel code]%1$cmov rsi, 10%1$cmov rdx, 34%1$clea rcx, [rel code]%1$ccall _printf%1$c%1$cmov rsp, rbp%1$cpop rbp%1$ccall end%1$c%1$cend:%1$cret%1$c", 0
 
 section .text
-    global start
-    global _main
+global start
+global _main
+extern _printf
 
 start:
-    call _main
-    ret
+call _main
+ret
 
 _main:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
+; commentaire inside
+push rbp
+mov rbp, rsp
 
-	mov edi, STDOUT
-	lea esi, [rel hello.string]
-    mov rdx, hello.len
-    mov rax, MACH_SYSCALL(WRITE)
-    syscall
+lea rdi, [rel code]
+mov rsi, 10
+mov rdx, 34
+lea rcx, [rel code]
+call _printf
 
-    leave
-    ret
+mov rsp, rbp
+pop rbp
+call end
+
+end:
+ret
