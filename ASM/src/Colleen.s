184d4d39bb01fc91aafe2e5ce87c515039d860c1
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-07-19 12:10:13 +0200

    First version of ASM part

---
+++ ASM/src/Colleen.s
@@ -0,0 +1,31 @@
+; SYSCALL
+%define MACH_SYSCALL(nb)        0x2000000 | nb
+%define STDOUT                  1
+%define WRITE                   4
+
+section .data
+hello:
+    .string db "Hello World!", 10
+    .len equ $ - hello.string
+
+section .text
+    global start
+    global _main
+
+start:
+    call _main
+    ret
+
+_main:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+
+	mov edi, STDOUT
+	lea esi, [rel hello.string]
+    mov rdx, hello.len
+    mov rax, MACH_SYSCALL(WRITE)
+    syscall
+
+    leave
+    ret
