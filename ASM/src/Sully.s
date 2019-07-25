f9fca788a040f8d8a9ce8d08c77fbb8301988cdf
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-07-25 13:10:26 +0200

    version of sully

--- ASM/src/Sully.s
+++ ASM/src/Sully.s
@@ -8,8 +8,10 @@ compile: db "nasm -f macho64 Sully_%1$d.s -o Sully_%1$d.o", 0
 link: db "ld Sully_%1$d.o -macosx_version_min 10.8 -lSystem -o Sully_%1$d", 0
 execute: db "./Sully_%d", 0
 code: db "section .text", 10, "global start", 10, "global _main", 10, "start: call _main", 10, "ret", 10, "_main: mov rdi, rsi", 0
+sully_str: db "/Sully", 0
 
-format: db "rslt = %s", 10, 0
+format_nb: db "nb = %d", 10, 0
+format: db "str = %s", 10, 0
 
 section .text
 global start
@@ -18,6 +20,8 @@ extern _system
 extern _asprintf
 extern _dprintf
 extern _free
+extern _strrchr
+extern _strcmp
 
 start:
 call _main
@@ -26,31 +30,46 @@ ret
 _main:
 push rbp
 mov rbp, rsp
-; sub rsp, 96
+sub rsp, 96
+
+mov byte[rsp], 5
+; CPT
+mov rdi, [rbp + 32]
+mov rsi, '/'
+call _strrchr
+cmp rax, 0
+je end
+mov rdi, rax
+mov rsi, sully_str
+call _strcmp
+
+cmp rax, 0
+movzx r8, byte[rsp]
+dec r8
+mov [rsp], r8
 
 mov rdi, 1
-mov rsi, format
-mov rdx, [rsp + 8]
-; mov rdx, [rdx]
+mov rsi, format_nb
+mov rdx, [rsp]
 call _dprintf
 
 ; INIT
-; mov byte[rsp], 5
+sully:
 ; lea rdi, [rel rsp + 16]
 ; mov rsi, filename
-; mov rdx, [rsp]
+; movzx rdx, byte[rsp]
 ; call _asprintf
 ; lea rdi, [rel rsp + 32]
 ; mov rsi, compile
-; mov rdx, [rsp]
+; movzx rdx, byte[rsp]
 ; call _asprintf
 ; lea rdi, [rel rsp + 48]
 ; mov rsi, link
-; mov rdx, [rsp]
+; movzx rdx, byte[rsp]
 ; call _asprintf
 ; lea rdi, [rel rsp + 64]
 ; mov rsi, execute
-; mov rdx, [rsp]
+; movzx rdx, byte[rsp]
 ; call _asprintf
 ; ; OPEN
 ; mov rdi, [rsp + 16]
@@ -58,7 +77,7 @@ call _dprintf
 ; mov rdx, 0644o
 ; mov rax, OPEN_SYSCALL
 ; syscall
-; jc .end
+; jc end
 ; mov [rsp + 80], rax
 ; ; WRITE
 ; mov rdi, [rsp + 80]
@@ -87,7 +106,7 @@ call _dprintf
 ; mov rdi, [rsp + 64]
 ; call _free
 
-.end:
+end:
 mov rsp, rbp
 pop rbp
 ret
