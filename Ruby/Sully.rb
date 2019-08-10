cbe00bc375be54615e6bc1e7ebfafd6f6acadd7c
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-08-10 17:01:23 +0200

    Fix Sully ruby part

--- Ruby/Sully.rb
+++ Ruby/Sully.rb
@@ -8,10 +8,10 @@ def write_in_file(file, s, cpt)
   end
 end
 
-s = "def write_in_file(file, s, cpt)\n  begin\n    file = File.open(file, 'w')\n    file.printf(s, s.inspect, cpt)\n    file.close\n  rescue\n    exit\n  end\nend\n\ns = %s\ncpt = %d\nbegin\n  tmp = File.open('Sully_5.rb', 'r')\n  cpt -= 1\n  tmp.close\nrescue\nend\nnew_file = \"Sully_\#{cpt}.rb\"\nwrite_in_file(new_file, s, cpt)\nexec(\"ruby \#{new_file}\") unless cpt <= 0\n"
+s = "def write_in_file(file, s, cpt)\n  begin\n    file = File.open(file, 'w')\n    file.printf(s, s.inspect, cpt)\n    file.close\n  rescue\n    exit\n  end\nend\n\ns = %s\ncpt = %d\nbegin\n  tmp = File.open(\"Sully_\#{cpt}.rb\", 'r')\n  cpt -= 1\n  tmp.close\nrescue\nend\nnew_file = \"Sully_\#{cpt}.rb\"\nwrite_in_file(new_file, s, cpt)\nexec(\"ruby \#{new_file}\") unless cpt <= 0\n"
 cpt = 5
 begin
-  tmp = File.open('Sully_5.rb', 'r')
+  tmp = File.open("Sully_#{cpt}.rb", 'r')
   cpt -= 1
   tmp.close
 rescue
